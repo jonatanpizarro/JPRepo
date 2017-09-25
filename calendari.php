63d805cf2ee2a467026f20eb6d93f545eba5e51e
jonatan <jpizarroserrano@iesesteveterradas.cat>
2017-09-25 17:24:20 +0200

    dias

---
+++ calendari.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+
+
+<head>
+</head>
+
+<body>
+	<table border="1">
+		<tr>
+			<td>Lunes</td>
+			<td>Martes</td>
+			<td>Miercoles</td>
+			<td>Jueves</td>
+			<td>Viernes</td>
+			<td>Sabado</td>
+			<td>Domingo</td>
+		
+		</tr>
+	
+	</table>
+
+</body>
